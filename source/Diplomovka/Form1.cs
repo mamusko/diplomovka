aadec4027895a5cea7a6126129843670adbe0036
mamusko <mamusko9@gmail.com>
2017-05-18 21:03:10 +0200

    Add files via upload

---
+++ source/Diplomovka/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using NAudio;
+
+namespace Diplomovka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+
+            synth.Speak(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "MP3 File (*.mp3|*.mp3;";
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+
+
+            NAudio.Wave.WaveStream pcm = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(new NAudio.Wave.Mp3FileReader(dialog.FileName));
+            NAudio.Wave.BlockAlignReductionStream stream = new NAudio.Wave.BlockAlignReductionStream(pcm);
+
+            NAudio.Wave.DirectSoundOut output = new NAudio.Wave.DirectSoundOut();
+            output.Init(stream);
+            output.Play();
+
+
+
+        }
+    }
+}
